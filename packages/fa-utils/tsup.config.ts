0204d5177237e90978722438df1ea9a35b7157d1
hamidrezakz <dimah.kazemzadeh.2@gmail.com>
2026-02-28 01:19:47 +0330

    update build config

--- packages/fa-utils/tsup.config.ts
+++ packages/fa-utils/tsup.config.ts
@@ -3,10 +3,14 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"],
+  target: "esnext",
   dts: true,
   sourcemap: true,
   clean: true,
   splitting: false,
   treeshake: true,
+  minify: true,
+  outDir: "dist",
+  skipNodeModulesBundle: true,
   external: [/^[^./]/],
 });
